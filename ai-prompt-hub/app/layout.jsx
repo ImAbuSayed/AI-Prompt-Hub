4d48a9e1e9712a821fa738f1465da7074f68e9c9
ImAbuSayed <imabusayed@hotmail.com>
2023-11-16 06:21:36 -0700

    Added meta data for homepage

---
+++ ai-prompt-hub/app/layout.jsx
@@ -0,0 +1,14 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Ai Prompt Hub",
+    description: "AI Prompt Hub is an open-source project that provides a platform for users from around the world to share and discover AI prompts. Whether you're a developer, a creative writer, or simply curious about AI, this platform allows you to contribute and explore a wide range of prompts. Join the community and unleash your creativity!"
+}
+
+const layout = () => {
+  return (
+    
+  )
+}
+
+export default layout
