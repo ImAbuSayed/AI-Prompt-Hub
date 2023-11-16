aee87cf06601df1194b02537573d1ff65666208e
ImAbuSayed <imabusayed@hotmail.com>
2023-11-16 07:15:45 -0700

    Minor Changes
    Added Required JSX Pages and Add some content  to the Home Page

--- ai-prompt-hub/app/layout.jsx
+++ ai-prompt-hub/app/layout.jsx
@@ -5,10 +5,19 @@ export const metadata = {
     description: "AI Prompt Hub is an open-source project that provides a platform for users from around the world to share and discover AI prompts. Whether you're a developer, a creative writer, or simply curious about AI, this platform allows you to contribute and explore a wide range of prompts. Join the community and unleash your creativity!"
 }
 
-const layout = () => {
+const RootLayout = ({ children }) => {
   return (
-    
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+          <main className="app">
+            {children}
+          </main>
+      </body>
+    </html>
   )
 }
 
-export default layout
+export default RootLayout
--- ai-prompt-hub/app/page.jsx
+++ ai-prompt-hub/app/page.jsx
@@ -1,7 +1,17 @@
 
+
 const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+      Discover and Share Best Prompts at
+      <br />
+      <span className="orange_gradient text-center">AI Prompt Hub</span>
+      </h1>
+      <p className="desc text-center">
+      AI Prompt Hub is an open-source project that provides a platform for users from around the world to share and discover AI prompts. Whether you're a developer, a creative writer, or simply curious about AI, this platform allows you to contribute and explore a wide range of prompts. Join the community and unleash your creativity!
+      </p>
+    </section>
   )
 }
 
