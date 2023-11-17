fc4219690c144c0d0f1695485a8b06cf95d258d3
ImAbuSayed <imabusayed@hotmail.com>
2023-11-16 22:49:25 -0700

    Added Some Button
    Just in frontend! No functionality added yet

--- ai-prompt-hub/components/Nav.jsx
+++ ai-prompt-hub/components/Nav.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -17,6 +19,23 @@ const Nav = () => {
           className="object-contain"
           />
       </Link>
+
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/creat-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button className="outline_btn" type="button" onClick={signOut}>
+            Sign Out
+            </button>
+          </div>
+        ):(
+          <>
+          </>
+        )}
+      </div>
+
     </nav>
   )
 }
