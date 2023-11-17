2b36e5c78d87c6efc8c724ca53160ff94522b609
ImAbuSayed <imabusayed@hotmail.com>
2023-11-17 04:33:06 -0700

    Fixed Name of Our Project
    Fixed Name of Our Project Because of Domain Reservation Issues

---
+++ ai-prompts-hub/app/page.jsx
@@ -0,0 +1,20 @@
+import Feed from '@components/Feed';
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+      Discover and Share Best Prompt at
+      <br />
+      <span className="orange_gradient text-center">AI Prompts Hub</span>
+      </h1>
+      <p className="desc text-center">
+      AI Prompts Hub is an open-source project that provides a platform for users from around the world to share and discover AI prompts. Whether you're a developer, a creative writer, or simply curious about AI, this platform allows you to contribute and explore a wide range of prompts. Join the community and unleash your creativity!
+      </p>
+      
+      <Feed />
+    </section>
+  )
+}
+
+export default Home
