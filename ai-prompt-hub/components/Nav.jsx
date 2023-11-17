086f27b07ba5ffa7e38aa55536416141ed4e2b3b
ImAbuSayed <imabusayed@hotmail.com>
2023-11-16 23:40:12 -0700

    profile option

--- ai-prompt-hub/components/Nav.jsx
+++ ai-prompt-hub/components/Nav.jsx
@@ -8,6 +8,13 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
   const isUserLoggedIn = true;
 
+  const [ providers, setProviders ] = useState(null);
+
+  useEffect(() => {
+    
+  }, [])
+  
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -29,6 +36,16 @@ const Nav = () => {
             <button className="outline_btn" type="button" onClick={signOut}>
             Sign Out
             </button>
+
+            <Link href="/profile">
+              <Image 
+              src="/assets/images/logo.svg"
+              width={36}
+              height={36}
+              className="rounded-full"
+              alt="profile"
+              />
+            </Link>
           </div>
         ):(
           <>
